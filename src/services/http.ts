bb73e3ad8823f3e0180744664ca31fb41bf31202
DevExpTeam <Viktor@outlook.com>
2024-10-19 20:24:07 -0700

    api port changed

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const http = axios.create({
-  baseURL: process.env.REACT_APP_API || 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_API || 'http://localhost:8010',
   headers: {
     'Content-Type': 'application/json'
   }
